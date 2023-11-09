8462ce1e19f785add051a1f53aaaabc5182d1db4
mclee <mclee@ablyblack.com>
2023-11-09 11:37:35 +0900

    Change minimum iOS version 14 to 13

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "BackgroundRemoval",
     platforms: [
 //        .macOS(.v11), .iOS(.v14)
-        .iOS(.v14)
+        .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
